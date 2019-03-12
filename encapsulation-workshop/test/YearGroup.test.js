5a79b336da495a4d5d532fbdbafa4c6f359b104e
PaulElcampeon <pauloladele@live.co.uk>
2019-03-12 10:46:52 +0000

    first test passes

--- encapsulation-workshop/test/YearGroup.test.js
+++ encapsulation-workshop/test/YearGroup.test.js
@@ -1,9 +1,13 @@
 const chai = require('chai')
-const expect = chai.expect;4
+const expect = chai.expect;
 
 const YearGroup = require('../classes/YearGroup.js');
 
 describe('YearGroup', ()=>{
 
+    let myYearGroup = new YearGroup();
     
+    it('should create an object instantiated from the class', ()=>{
+        expect(myYearGroup).to.be.an.instanceOf(YearGroup);
+    })
 })
