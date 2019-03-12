ae0efffc770e332b19a619dfc8a2686b3cf6da47
PaulElcampeon <pauloladele@live.co.uk>
2019-03-12 11:50:20 +0000

    1st test now passes

---
+++ encapsulation-workshop/classes/Teacher.js
@@ -0,0 +1,10 @@
+class Teacher {
+    constructor(name, initials, subject) {
+        this.name = name;
+        this.initials = initials;
+        this.subject = subject;
+        this.yearsOfService = 0;
+    }
+}
+
+module.exports = Teacher;
--- encapsulation-workshop/test/Teacher.test.js
+++ encapsulation-workshop/test/Teacher.test.js
@@ -8,10 +8,9 @@ describe('Teacher', ()=>{
     let newTeacher = new Teacher("Sally", "S.O", "Maths");
     
     it('should create a Teacher object with all relevant fields', ()=>{
-        expect(newTeacher.yearsOfService).equals(1);
+        expect(newTeacher.yearsOfService).equals(0);
         expect(newTeacher.name).equals("Sally");
         expect(newTeacher.initials).equals("S.O");
         expect(newTeacher.subject).equals("Maths");
-
     })
 })
